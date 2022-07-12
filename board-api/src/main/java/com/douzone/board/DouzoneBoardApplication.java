7f9d1ef24c262d1bc83cba4d5ddd71ef3c0fbbd2
ehek01 <whdgus0515@gmail.com>
2022-07-12 20:22:49 +0900

    init commit

---
+++ board-api/src/main/java/com/douzone/board/DouzoneBoardApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouzoneBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DouzoneBoardApplication.class, args);
+	}
+
+}
---
+++ board-api/src/test/java/com/douzone/board/DouzoneBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DouzoneBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

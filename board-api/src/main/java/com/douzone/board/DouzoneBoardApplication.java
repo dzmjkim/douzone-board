0f21f157a25e3ab4b7a33d4eff92766cd6f6447d
ehek01 <whdgus0515@gmail.com>
2022-07-17 17:19:46 +0900

    user 관련 수정 끝(종현기준)

--- board-api/src/main/java/com/douzone/board/DouzoneBoardApplication.java
+++ board-api/src/main/java/com/douzone/board/DouzoneBoardApplication.java
@@ -28,7 +28,7 @@ public class DouzoneBoardApplication {
 //			userService.saveRole(new Role(null, "ROLE_ADMIN"));
 //
 //			// user table init
-//			userService.saveUser(new User(null, "user", "jonghyunLim", "1",1));
+//			userService.saveUser(new User(null, "user", "jonghyunLim", "1", 1, "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2xvZ2luIiwiZXhwIjoxNjU4OTA1MjA0fQ.5zq7_4KQ_cltnD_aTTZAqFyKkwShlGsCQi4c5XgQVks"));
 //
 //			// role_user table init
 //			userService.addRoleToUser("user", "ROLE_USER");

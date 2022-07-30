7187e2cd32305fdadd908b395dc89cd7d3f2148e
ehek01 <whdgus0515@gmail.com>
2022-07-30 20:07:34 +0900

    단일 게시물 조회기능 추가

--- board-api/src/main/java/com/douzone/board/web/api/PostApiController.java
+++ board-api/src/main/java/com/douzone/board/web/api/PostApiController.java
@@ -8,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
@@ -49,5 +46,13 @@ public class PostApiController {
 
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/board").toUriString());
         return ResponseEntity.created(uri).build();
+    }
+
+    /**
+     * 단일 게시글 조회
+     */
+    @GetMapping("/board/{id}")
+    public ResponseEntity<PostDto> getPost(@PathVariable Long id) {
+        return ResponseEntity.ok().body(postService.findById(id));
     }
 }
